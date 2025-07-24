cb61ed1e0e49c53ce0dc268e5bc40b78d9ab2a7f
RaheelDev1 <myselfraheelabbas@gmail.com>
2025-07-24 07:00:45 +0100

    Initialized Node project with Express and basic server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Sales Lead Tracker API is running...');
+});
+app.listen(PORT, () => {
+    app.listen(PORT, () => {
+        console.log('Server running on port ${PORT}');
+
+    });
+});
