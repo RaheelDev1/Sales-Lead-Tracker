11cc076c8b7f7d3b18dc744fb0dc018f8402445c
RaheelDev1 <myselfraheelabbas@gmail.com>
2025-07-25 07:08:30 +0100

    Added GET /leads endpoint to fetch all sales leads

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
+const fs = require('fs');
+const DATA_FILE = './leads.json';
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Sales Lead Tracker API is running...');
+});
+app.get('/leads', (req, res) => {
+    if (err) {
+        return res.status(500).json({message: 'Error reading leads data'});
+    }
+    const leads = JSON.parse(data || '[]');
+    res.json(leads);
+    
 });
 app.listen(PORT, () => {
     app.listen(PORT, () => {
